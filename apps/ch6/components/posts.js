50263ad10ef4c57dff4406e6d142122d7f0b8e14
hwani3142 <hwani3142@gmail.com>
2024-12-29 00:28:49 +0900

    lecture(181): bind, optimistic update, cache revalidatePath

--- apps/ch6/components/posts.js
+++ apps/ch6/components/posts.js
@@ -1,25 +1,34 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
+"use client";
 
-function Post({ post }) {
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { togglePostLikeStatue } from "@/actions/posts";
+import { useOptimistic } from "react";
+
+function Post({ post, action }) {
   return (
-    <article className="post">
-      <div className="post-image">
+    <article className='post'>
+      <div className='post-image'>
         <img src={post.image} alt={post.title} />
       </div>
-      <div className="post-content">
+      <div className='post-content'>
         <header>
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
             </p>
           </div>
           <div>
-            <LikeButton />
+            <form
+              action={action.bind(null, post.id)}
+              className={post.isLiked ? "liked" : ""}
+            >
+              <LikeButton />
+            </form>
           </div>
         </header>
         <p>{post.content}</p>
@@ -29,15 +38,41 @@ function Post({ post }) {
 }
 
 export default function Posts({ posts }) {
-  if (!posts || posts.length === 0) {
+  const [optimisticPost, updateOptimisticPosts] = useOptimistic(
+    posts,
+    (prevPosts, updatedPostId) => {
+      const updatedPostIndex = prevPosts.findIndex(
+        (post) => post.id === updatedPostId
+      );
+
+      if (updatedPostIndex === -1) {
+        return prevPosts;
+      }
+
+      const updatedPost = { ...prevPosts[updatedPostIndex] };
+      updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+      updatedPost.isLiked = !updatedPost.isLiked;
+      const newPosts = [...prevPosts];
+      newPosts[updatedPostIndex] = updatedPost;
+
+      return newPosts;
+    }
+  );
+
+  if (!optimisticPost || optimisticPost.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
   }
 
+  async function updatePost(postId) {
+    updateOptimisticPosts(postId);
+    await togglePostLikeStatue(postId);
+  }
+
   return (
-    <ul className="posts">
-      {posts.map((post) => (
+    <ul className='posts'>
+      {optimisticPost.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <Post post={post} action={updatePost} />
         </li>
       ))}
     </ul>
--- docs/ch6/README.md
+++ docs/ch6/README.md
@@ -13,3 +13,33 @@
   - 함수 안에서 `"use server"` 를 선언 + async function 으로 전환. server action 임을 알림.
 - `formData`
   - name attribute 로 전달된 값들을 파라미터로 전달받아 처리 가능함
+- `<button formAction={} />`
+  - 버튼의 formAciton 프로퍼티로도 받을 수 있음
+
+## server-only
+
+- `use server` 는 서버 액션이 되어야 하는것을 서버에게 말해주는 정도의 역할임
+  - 즉, 코드가 서버에서만 실행된다는 것을 의미하거나 보장하지 않음
+- 클라이언트 측에서 절대 실행되어서는 안되는 코드가 있다면, `server-only` package 사용
+  - `npm install server-only`
+  - `import 'server-only'`
+
+## bind
+
+- 바인드는 보통 함수를 특정 컴포넌트에 바인딩시킬때 사용
+- 함수 자체를 미리 생성해두고 호출되는 시점에 함수를 전달해줄 수도 있음
+  - 이 점을 활용하여, 함수 레퍼런스를 인자로 넘겼을 때 인자를 넘겨줄 방법으로 사용할 수 있음
+  - `action={method.bind(null, 호출될때넘겨줄인자1)}`
+  - `function method(인자1) {...}`
+
+## optimistic update
+
+- `useOptimistic(대상, (이전상태, 인자값) => {})`
+- 업데이트가 성공일거라 믿을 때 미리 상태를 업데이트하고 백그라운드에서 데이터 업데이트를 수행
+  - 즉, 클라이언트가 서버에 응답을 받지 않고도 낙관적으로 요청이 성공적으로 끝났다고 가정함
+
+## 이미지 업로드 테스트
+
+- cloudinary 사용해서 이미지를 업로드하고 URL 을 받아 처리
+  - `npm install cloudinary`
+  - https://cloudinary.com/
