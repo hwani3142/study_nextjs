ed1bd5393a752cdbe44ac42030d7c7dd66bfbe93
hwani3142 <hwani3142@gmail.com>
2025-02-03 16:03:15 +0900

    lecture(343): section17. project for API routes

---
+++ apps/ch17/helpers/db-util.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  return await MongoClient.connect(
+    "mongodb+srv://jh:loobWKvtuIMtXOsE@cluster0.uhj39.mongodb.net/events?retryWrites=true&w=majority&appName=Cluster0"
+  );
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+  const result = await db.collection(collection).insertOne(document);
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort, filter = {}) {
+  const db = client.db();
+
+  const documents = await db
+    .collection(collection)
+    .find(filter)
+    .sort(sort)
+    //   .sort({ _id: -1 }) // descending
+    .toArray();
+
+  return documents;
+}
---
+++ docs/ch17/README.md
@@ -0,0 +1,7 @@
+# Section17. Practice on API route
+
+## mongoDB
+
+- mongodb atlas free tier(m0) 생성
+- `npm install mongodb`
+- 서버사이드에서만 처리. 클라이언트 처리 X
